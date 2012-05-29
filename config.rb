4b58f19d4275a0d44839622e90a138f07f2b58b0
Simon Jockers <hello@simonjockers.com>
2012-05-29 14:12:38 +0200

    SCSS support added

--- config.rb
+++ config.rb
@@ -7,13 +7,23 @@ sass_dir = "content/styles"
 images_dir = "output/images"
 javascripts_dir = "output/javascripts"
 
+sass_options = {
+  :syntax => :scss
+}
+
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
-preferred_syntax = :sass
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
